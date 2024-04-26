afae3a6562876d40d10b20842d705e2373216c87
Precious Olusola <preciousolusola16@gmail.com>
2024-04-26 16:48:33 +0100

    Updated site titling

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ import siteData from "@/data/site.json";
 export default function Document() {
     return (
         <Html lang="en">
-            <title>{siteData.siteTitle} :: {siteData.siteDescription}</title>
+            <title>{siteData.siteTitle} {siteData.siteDescription}</title>
             <Head />
             <body>
                 <main className={`min-h-screen w-full bg-[#f7f7f9]`}>
